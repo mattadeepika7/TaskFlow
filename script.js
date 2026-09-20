1587c6a952e6fa86524e9efb8b1cb9b0bf27f893
Deepika <mattadeepika7@gmail.com>
2026-09-20 11:01:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+let tasks = [];
+let count = 0;
+
+
+// Add Task
+function addTask() {
+
+    let name = document.getElementById("task").value;
+
+    if (name == "") {
+        alert("Enter a task");
+        return;
+    }
+
+    tasks.push({
+        id: count,
+        name: name,
+        status: "todo"
+    });
+
+    count++;
+
+    document.getElementById("task").value = "";
+
+    showTasks();
+}
+
+
+// Show Tasks
+function showTasks() {
+
+    document.getElementById("todo").innerHTML = "";
+    document.getElementById("progress").innerHTML = "";
+    document.getElementById("completed").innerHTML = "";
+
+    for (let i = 0; i < tasks.length; i++) {
+
+        let task = document.createElement("div");
+
+        task.className = "task";
+        task.draggable = true;
+
+        task.innerHTML =
+            tasks[i].name +
+            "<br>" +
+            "<button class='edit' onclick='editTask(" + i + ")'>Edit</button>" +
+            "<button class='delete' onclick='deleteTask(" + i + ")'>Delete</button>";
+
+        task.ondragstart = function(event) {
+            event.dataTransfer.setData("index", i);
+        };
+
+        document.getElementById(tasks[i].status).appendChild(task);
+    }
+}
+
+
+// Edit
+function editTask(i) {
+
+    let name = prompt("Enter new task:", tasks[i].name);
+
+    if (name != null && name != "") {
+        tasks[i].name = name;
+        showTasks();
+    }
+}
+
+
+// Delete
+function deleteTask(i) {
+
+    tasks.splice(i, 1);
+
+    showTasks();
+}
+
+
+// Allow Drop
+function allowDrop(event) {
+
+    event.preventDefault();
+}
+
+
+// Drop
+function drop(event, status) {
+
+    event.preventDefault();
+
+    let index = event.dataTransfer.getData("index");
+
+    tasks[index].status = status;
+
+    showTasks();
+}
